76bf7ecd8b5ce432a4bd0b4da2984467260f3fbd
Tabatha <tabatha.sanchez193@tectijuana.edu.mx>
2020-07-12 16:06:55 -0700

    Clase Poner Atencion,FINAL

---
+++ EjercicioPonerAtencion.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+import java.util.ArrayList;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import java.awt.Dimension;
+import java.awt.Canvas;
+
+public class EjercicioPonerAtencion extends Canvas{
+
+	public EjercicioPonerAtencion(){
+		//this.setResizable(false);
+		this.setBounds(350,90,690,870);
+		//this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		this.setVisible(true);	
+
+
+	}	
+}
